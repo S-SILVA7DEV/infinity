fbf7eb797091cc42f770e8d543c44e442052061b
StevenOliveira <75679370+S-SILVA7DEV@users.noreply.github.com>
2026-07-19 21:11:02 -0300

    .

--- js/app.js
+++ js/app.js
@@ -4,13 +4,14 @@
 
 console.log("Infinity iniciado!");
 
-/*======================
+/*====================== 
     Elementos
 =======================*/
 const startButton = document.querySelector(".start-button");
 const welcomeScreen = document.querySelector("#welcome-screen");
 const loadingScreen = document.querySelector("#loading-screen");
-
+const app = document.querySelector("#app");
+const counter = document.querySelector("#counter");
 /*===================
     Eventos
 =====================*/
@@ -22,11 +23,34 @@ startButton.addEventListener("click", startInfinity);
 function startInfinity(){
      welcomeScreen.style.display = "none";
      loadingScreen.style.display = "flex";
+
      setTimeout(function(){
-        console.log("Hora de abrir o aplicarivo!");
+        loadingScreen.style.display = "none";
+        app.style.display = "flex";
+        updateCounter(); 
     
      },2000);
 }
 
+function updateCounter(){
+    const startDate = new Date("2026-04-17");
+    const today = new Date();
+
+    const difference = today - startDate;
+    const days = Math.floor(difference / 86400000);
+    console.log(days);
+
+    let word = "dias";
+    if(days <0) {
+        counter.textContent = "❤️ Nosso amor começa em breve...";
+        return;
+    }
+
+    if (days ===1){
+        word = "dia";
+    }
+    counter.textContent =  `❤️ ${days} ${word} juntos`;
+
+}
 
  
